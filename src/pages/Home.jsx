99f3f8e4158d3cfba31193904dc1ee6483979394
adrianapquijanoc <adrianapquijano>
2025-09-17 10:40:33 -0500

    Versión inicial de Kodigo Music SPA-v6

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { Carousel } from "react-bootstrap"; 
 import { Link } from "react-router-dom";
+import { Carousel } from "react-bootstrap"; 
 
 function Home() {
   const musicaDelMomento = [
